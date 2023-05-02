479f6fef9aaf7da1d0f2fe659d269b264d952e78
Bruno Tomé <ibrunotome@gmail.com>
2023-05-01 21:57:08 -0300

    feat: allow multiple terms

--- api/index.py
+++ api/index.py
@@ -5,27 +5,28 @@ import os
 
 app = Flask(__name__)
 
-def getFuturePublicTenders(term, states):
-    response = requests.get(f'https://www.pciconcursos.com.br/pesquisa/?q={term}')
-    soup = BeautifulSoup(response.text, 'html.parser')
-    future_public_tenders = soup.find_all('div', class_='fa')
+def getFuturePublicTendersFromPciConcursos(terms, states):
+    for term in terms:
+        response = requests.get(f'https://www.pciconcursos.com.br/pesquisa/?q={term}')
+        soup = BeautifulSoup(response.text, 'html.parser')
+        future_public_tenders = soup.find_all('div', class_=['fa', 'na'])
 
-    if not len(future_public_tenders):
-        return ''
+        if not len(future_public_tenders):
+            return ''
 
-    message = ''
-    for line in future_public_tenders:
-        title = line.find('a').get_text()
-        state = line.find('div', class_='cc').get_text()
-        link = line.find('a').get('href')
-        slots = str(line.find('div', class_='cd')).replace('<br>', ' ').replace('</br>', '').replace('<br/>', '\n').replace('<span>', '').replace('</span>', '').replace('<div class="cd">', '').replace('</div>', '')
-        date = str(line.find('div', class_='ce').find('span')).replace('<br>', ' ').replace('</br>', '').replace('<span>', '').replace('</span>', '')
+        message = ''
+        for line in future_public_tenders:
+            title = line.find('a').get_text()
+            state = line.find('div', class_='cc').get_text()
+            link = line.find('a').get('href')
+            slots = str(line.find('div', class_='cd')).replace('<br>', ' ').replace('</br>', '').replace('<br/>', '\n').replace('<span>', '').replace('</span>', '').replace('<div class="cd">', '').replace('</div>', '')
+            date = str(line.find('div', class_='ce').find('span')).replace('<br>', ' ').replace('</br>', '').replace('<span>', '').replace('</span>', '')
 
-        if 'Militar' in title:
-            continue
-    
-        if state in states:
-            message += f'''
+            if 'Militar' in title:
+                continue
+        
+            if state in states:
+                message += f'''
 {state} {date} - {title}
 {slots}
 {link}
@@ -48,13 +49,13 @@ def sendTelegramMessage(message):
 
 @app.route('/')
 def home():
-    return 'Bot Telegram para concursos públicos abertos de PC/PRF/PF'
+    return 'Bot Telegram para concursos públicos abertos de PC/PRF/PF e Guarda Municipal'
 
 @app.route('/concursos')
 def concursos():
-    term = 'policia'
-    states = ['SP', 'SC', 'MS', 'PR', 'MG']
-    message = getFuturePublicTenders(term, states)
+    terms = ['policia', 'guarda municipal']
+    states = ['SP', 'SC', 'MS', 'PR']
+    message = getFuturePublicTendersFromPciConcursos(terms, states)
     
     if message:
         sendTelegramMessage(message)
