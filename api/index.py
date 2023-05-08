30c81222a046d794db1811aaac82456ca44f8eee
Bruno Tomé <ibrunotome@gmail.com>
2023-05-08 13:21:28 -0300

    fix: early return when using multiple terms

--- api/index.py
+++ api/index.py
@@ -5,26 +5,29 @@ import os
 
 app = Flask(__name__)
 
+
 def getFuturePublicTendersFromPciConcursos(terms, states):
+    message = ''
+
     for term in terms:
         response = requests.get(f'https://www.pciconcursos.com.br/pesquisa/?q={term}')
         soup = BeautifulSoup(response.text, 'html.parser')
         future_public_tenders = soup.find_all('div', class_=['fa', 'na'])
 
         if not len(future_public_tenders):
-            return ''
+            continue
 
-        message = ''
         for line in future_public_tenders:
             title = line.find('a').get_text()
             state = line.find('div', class_='cc').get_text()
             link = line.find('a').get('href')
             slots = str(line.find('div', class_='cd')).replace('<br>', ' ').replace('</br>', '').replace('<br/>', '\n').replace('<span>', '').replace('</span>', '').replace('<div class="cd">', '').replace('</div>', '')
-            date = str(line.find('div', class_='ce').find('span')).replace('<br>', ' ').replace('</br>', '').replace('<span>', '').replace('</span>', '')
+            date = str(line.find('div', class_='ce').find('span')).replace(
+                '<br>', ' ').replace('</br>', '').replace('<span>', '').replace('</span>', '')
 
             if 'Militar' in title:
                 continue
-        
+
             if state in states:
                 message += f'''
 {state} {date} - {title}
@@ -35,6 +38,7 @@ def getFuturePublicTendersFromPciConcursos(terms, states):
 '''
 
     return message
+
 
 def sendTelegramMessage(message):
     botToken = os.environ['BOT_TOKEN']
@@ -47,16 +51,18 @@ def sendTelegramMessage(message):
     server = requests.post(url, data=form_data)
     return server.text
 
+
 @app.route('/')
 def home():
     return 'Bot Telegram para concursos públicos abertos de PC/PRF/PF e Guarda Municipal'
+
 
 @app.route('/concursos')
 def concursos():
     terms = ['policia', 'guarda municipal']
     states = ['SP', 'SC', 'MS', 'PR']
     message = getFuturePublicTendersFromPciConcursos(terms, states)
-    
+
     if message:
         sendTelegramMessage(message)
         return message
